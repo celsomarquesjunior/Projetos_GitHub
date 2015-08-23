c1e7a6275d1d14951dba1078b614efcee60959b4
celsomarquesjunior <celsomarques.junior@gmail.com>
2015-08-22 23:57:35 -0600

    atual_calc

---
+++ calculadora.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" href="_css/estilo.css"/>
+	<title>Curso PHP.</title>
+</head>
+<body>
+<div>
+	<?php
+		$pv = $_GET["a"];
+		$sv = $_GET["b"];
+		$op = $_GET["op"];
+		$v = $pv + $sv;
+		for 
+		
+		
+		
+		
+		echo "O resultado entre os valores é: <h2>$v</h2>";
+		
+	?>
+	<a class="botao" href="calculadora.html">Voltar</a>
+</div>	
+</body>
+</html>	
